4c17dbc01b0b52283347579dd2d1893744655333
izul <izlwhd@gmail.com>
2023-12-06 21:47:09 +0700

    home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="max-w-3xl mx-[auto] border-2 rounded min-h-screen flex-wrap text-center">
+    <h1 class="text-4xl">Grably</h1>
+    <table class="table-fixed text-left">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Query</th>
+            <th>Example</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Movie v1</td>
+            <td>/movie/v1/[TMDB_ID]</td>
+            <td>/movie/v1/tt0439572</td>
+          </tr>
+          <tr>
+            <td>TV Series v1</td>
+            <td>/tv/v1/[TMDB_ID]/S[]/E[]</td>
+            <td>/tv/v1/1396/S1/E1</td>
+          </tr>
+        </tbody>
+      </table>
+</div>
